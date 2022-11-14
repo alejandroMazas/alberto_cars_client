5ac91babaff2eacc093d092f630f5c1bcd95a684
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-11-14 14:33:48 +0100

    CreateCarForm

--- src/components/carForm/CreateCarForm.js
+++ src/components/carForm/CreateCarForm.js
@@ -1,13 +1,52 @@
+import { useState } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
+import carsService from '../../services/cars.services'
 import './CreateCarForm.css'
+import { useNavigate } from "react-router-dom"
 
 const CreateCarForm = () => {
+
+    const [carData, setCarData] = useState(
+        {
+            brand: "",
+            model: "",
+            image: "",
+            generation: "",
+            production: ""
+        }
+    )
+
+    const { brand, model, image, generation, production } = carData
+
+    const handleInput = (e) => {
+        const { name, value } = e.currentTarget
+
+        setCarData({
+            ...carData,
+            [name]: value
+        })
+    }
+
+    const navigate = useNavigate()
+
+    const handleSubmit = e => {
+
+        e.preventDefault()
+
+        carsService
+            .saveCar(carData)
+            .then(() => {
+                navigate('/ListaCompleta')
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <Container>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="brand">
                     <Form.Label>Marca</Form.Label>
-                    <Form.Control type="text" />
+                    <Form.Control type="text" value={brand} onChange={handleInput} name="brand" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -15,7 +54,7 @@ const CreateCarForm = () => {
 
                 <Form.Group className="mb-3" controlId="model">
                     <Form.Label>Modelo</Form.Label>
-                    <Form.Control type="text" />
+                    <Form.Control type="text" value={model} onChange={handleInput} name="model" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -23,7 +62,7 @@ const CreateCarForm = () => {
 
                 <Form.Group className="mb-3" controlId="image">
                     <Form.Label>Imagen</Form.Label>
-                    <Form.Control type="text" />
+                    <Form.Control type="text" value={image} onChange={handleInput} name="image" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -31,7 +70,7 @@ const CreateCarForm = () => {
 
                 <Form.Group className="mb-3" controlId="generation">
                     <Form.Label>Generación</Form.Label>
-                    <Form.Control type="number" />
+                    <Form.Control type="text" value={generation} onChange={handleInput} name="generation" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -39,7 +78,7 @@ const CreateCarForm = () => {
 
                 <Form.Group className="mb-3" controlId="production">
                     <Form.Label>Producción</Form.Label>
-                    <Form.Control type="text" />
+                    <Form.Control type="text" value={production} onChange={handleInput} name="production" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
